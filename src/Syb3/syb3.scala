50622921431755edb350263b0da368784dbd12bd
Tom <tom@intellicode.nl>
2010-10-30 13:37:17 +0200

    reverted back to scala

---
+++ src/Syb3/syb3.scala
@@ -0,0 +1,82 @@
+package Syb3;
+//package syb3
+
+object syb3 {
+  // Example where run-time composition of traits would be handy!
+  trait Data[ctx[_],a] {
+    self : ctx[a] =>
+    def me = self
+    
+    def gmapQ[r] : {def apply[b](x : b)(implicit dt : Data[ctx,b]) : r} => a => List[r]
+  }
+
+  abstract case class DataChar[ctx[_]]() extends Data[ctx,Char] {
+    self : ctx[Char] =>
+    
+    def gmapQ[r] = f => n => Nil
+  } 
+  
+  abstract case class DataList[ctx[_],a](implicit d : Data[ctx,a]) extends Data[ctx,List[a]] {
+    self : ctx[List[a]] =>
+    
+    def gmapQ[r] = f => {
+        case Nil    => Nil
+        case x::xs  => List(f(x)(d),f(xs)(this))
+    }
+  }
+  
+  trait Size[a] extends Data[Size,a] {
+    def gsize : a => Int = t =>
+      1 + sum(gmapQ[Int] (new {def apply[b](x:b)(implicit dt : Data[Size,b]) = dt.me.gsize(x)})(t))
+  }
+  
+  abstract case class SizeList[a]()(implicit d : Size[a]) extends DataList[Size,a]()(d) with Size[List[a]] {
+    override def gsize = {
+      case Nil => 0
+      case x::xs => d.gsize(x) + gsize(xs)
+    } 
+  }
+  
+  // inconvenient: Have to repeat code per each generic function.
+  implicit def sizeChar : Size[Char] = 
+    new DataChar[Size]() with Size[Char] 
+  
+  def alternativeList[a](implicit d : Size[a]) : Size[List[a]] = 
+    new DataList[Size,a]()(d) with Size[List[a]]
+  
+  implicit def sizeList2[a](implicit d : Size[a]) : Size[List[a]] = 
+    new SizeList[a]()(d) with Size[List[a]]
+  
+  // The following is not possible.
+  
+  /*
+  implicit def genericList[ctx[_],a](implicit d : ctx[a]) : Data[ctx,List[a]] = 
+    new DataList[Size,a]()(d) with ctx[List[a]]
+  */
+                                                
+  def test(implicit size : Size[Char]) = size.gsize('a')
+  def test2(implicit size : Data[Size,Char]) = size.me.gsize('a')
+  def test3(implicit size : Size[List[Char]]) = size.gsize(List('a','b'))
+  
+  def test4(implicit s1 : Size[Char], s2 : Size[List[Char]]) = 
+    (s1.gsize('a'),s2.gsize(List('a','b')))
+  
+  def sum(list: List[Int]) = list.foldLeft(0)(_+_)
+  
+   /*
+   trait A {def foo() : Int}
+   trait B {}
+   
+   trait C extends B with A
+   
+   def test(x : A with B) = x.foo()
+   
+   
+   trait T[A] {self : A => def me = self}
+   
+   trait TA extends T[TA] {self : TA => def foo() : Int}
+   
+   def test(x : T[TA]) = x.me.foo()
+   
+   trait T1[A] extends A*/
+}
---
+++ src/nl/cs/uu/syb/test.scala
@@ -0,0 +1,3 @@
+object Test {
+	val test = 1
+}
